4a89a7ac18c1e08fe032307bd630dfec9061527c
icodeforlife24 <shiviutkarsh@gmail.com>
2026-05-05 05:16:10 +0530

    Update route.js

--- itsp_team_making/app/api/ideas/route.js
+++ itsp_team_making/app/api/ideas/route.js
@@ -1,25 +1,28 @@
 import { prisma } from "@/lib/prisma";
 
 export async function POST(req) {
-  try {
-    const body = await req.json(); // ✅ MUST
+    try {
+        const body = await req.json(); // ✅ MUST
 
-    console.log("BODY RECEIVED:", body);
-    const idea = await prisma.idea.create({
-      data: {
-        title: body.title,
-        text: body.text,
-        flair: body.flair,
-        Contact: Number(body.contact),
-        Private: body.private,
-        user_LDAP: body.user.roll, // ✅ correct
-      },
-    });
-    console.log("BODY RECEIVED:", body);
-    return Response.json(idea);
-  } catch (err) {
-    console.error(err);
+        console.log("BODY RECEIVED:", body);
+        const idea = await prisma.idea.create({
+            data: {
+                title: body.title,
+                text: body.text,
+                flair: body.flair,
 
-    return Response.json({ error: "Server error" }, { status: 500 });
-  }
+                contact: body.contact ? Number(body.contact) : 0, // ✅ correct
+
+                isPrivate: Boolean(body.private), // ✅ correct
+
+                user_Ldap: body.user.roll, // ✅ correct
+            },
+        });
+        console.log("BODY RECEIVED:", body);
+        return Response.json(idea);
+    } catch (err) {
+        console.error(err);
+
+        return Response.json({ error: "Server error" }, { status: 500 });
+    }
 }
