c047ac44fcc919e7d8b3c334cab346dbcc6fbe21
icodeforlife24 <shiviutkarsh@gmail.com>
2026-05-05 02:59:12 +0530

    Update page.js

--- itsp_team_making/app/page.js
+++ itsp_team_making/app/page.js
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import Navbar from "@/components/Navbar";
 import IdeaCard from "@/components/IdeaCard";
-
+export const dynamic = "force-dynamic";
 export default async function Home() {
   const ideas = await prisma.idea.findMany({
     orderBy: { createdAt: "desc" },
